89465c5f56111d020e63b682dfeb5b1f0d831613
sskt0906 <sskt0906@gmail.com>
2025-06-27 16:34:21 +0900

    deploy: 25/6/27 ルーティングの修正

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Callback from './pages/Callback';
 import Home from './pages/Home';
+import Profile from './pages/Profile'; // ← 追加
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/callback" element={<Callback />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} /> {/* ← 追加 */}
       </Routes>
     </BrowserRouter>
   );
