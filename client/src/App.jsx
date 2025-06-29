aa8baf31fbd5e0baec51bf70b17d235550eb21f4
sskt0906 <sskt0906@gmail.com>
2025-06-30 00:35:29 +0900

    modify: App.jsx for SPA routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Callback from './pages/Callback';
 import Home from './pages/Home';
@@ -7,13 +7,13 @@ import Profile from './pages/Profile'; // ← 追加
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/callback" element={<Callback />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} /> {/* ← 追加 */}
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
